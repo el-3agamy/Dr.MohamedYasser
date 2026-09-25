6f26b82e42253970a462825e074093808e4ef7a3
saeid el-agamy <saeid25@gmail.com>
2026-09-26 01:38:46 +0300

    fix: use img instead of IMAGE

--- src/app/_components/Education/Certificate/Certificate.tsx
+++ src/app/_components/Education/Certificate/Certificate.tsx
@@ -15,10 +15,10 @@ export default function Certificate({item} :CertificateProps) {
           <figure  className="shrink-0 w-64 flex flex-col items-center text-center mx-4">
             {/* Wrapper div forcing 1:1 ratio */}
             <div className="w-[200px] h-[200px] relative rounded-full overflow-hidden border-4 border-white bg-amber-300 mx-auto hover:scale-105 transition-transform ease-in-out hover:cursor-pointer">
-              <Image
+              <img
                 src={item.src}
                 alt={item.title}
-                fill
+                // fill
                 className="object-cover"
               />
             </div>
